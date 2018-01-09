832c564208b7a94d2682b0c11c4451f9db8bac69
lamineDiankha <34652697+lamineDiankha@users.noreply.github.com>
2018-01-09 02:16:47 +0000

    Add files via upload

---
+++ proj.php
@@ -0,0 +1,5 @@
+SELECT obstacle , niveau_difficulter FROM obstacles
+SELECT idsoldat, Date FROM participation
+SELECT*FROM idsoldat
+SELECT MIN(note_min) AS obstacles FROM participantion
+SELECT COUNT(*) AS obstacles FROM 
